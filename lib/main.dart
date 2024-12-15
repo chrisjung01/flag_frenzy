aa7f734009a3d983b1d20493dce1d84bfd8a8fb4
Christian Jung <chris.j88@t-online.de>
2024-12-15 21:00:46 +0100

    Remove unused imports in main.dart to clean up the codebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
-import 'package:flag_frenzy/game/game_screen.dart';
 import 'package:flag_frenzy/widgets/menu_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
